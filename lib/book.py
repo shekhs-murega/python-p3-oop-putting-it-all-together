9d80232cbb97d917c0a0f604ebb8af682191ed8d
shekinah <nyamburamary1212@gmail.com>
2023-11-27 18:00:26 +0300

    Complete putting it all together task

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        if not isinstance(page_count, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, new_page_count):
+        if not isinstance(new_page_count, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = new_page_count
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+mY_BOOK = Book("my title","23")
+mY_BOOK.page_count = "34"
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        if not isinstance(size, int):
+            print("size must be an integer")
+        else:
+            self._size = size
+        self.condition = "New"
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, new_size):
+        if not isinstance(new_size, int):
+            print("size must be an integer")
+        else:
+            self._size = new_size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'
+
+    size = property(get_size, set_size)
+
+
+shoe_mine = Shoe("mama", "12")
